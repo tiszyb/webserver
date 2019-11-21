5c1c5cd8d5f2d5483b3430e79a2483a62ff1c7a4
Ibrahim SALAWUDEEN <isalawudeen@cscs.ng>
2019-11-21 18:58:37 +0100

    created form for weather forecast

--- src/app.js
+++ src/app.js
@@ -44,15 +44,15 @@ app.get('/weather', (req, res) => {
         })
     }   
     geocode(req.query.address, (error, {latitude, longitude, location} = {} ) => {
-        if(error) return res.send(error);
+        if(error) return res.send({error});
     
         forecast(latitude, longitude, (error, forecastData)=>{
-            if(error) return res.send(error);
+            if(error) return res.send({error});
     
             console.log(location)
             res.send({
                 location,
-                Forecast : forecastData
+                forecast : forecastData
             })       
         }) 
     });
